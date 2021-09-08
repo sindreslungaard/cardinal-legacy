49c7856111f746d1d35d5010cd0fb31b1687e2be
Sindre Slungaard <slungaards@gmail.com>
2021-09-08 04:15:18 +0200

    remove logo outline

--- webapp/src/components/Header.svelte
+++ webapp/src/components/Header.svelte
@@ -4,7 +4,7 @@ import Cardinal from "../assets/cardinal.png"
 
 <header class="flex-none p-4 w-full">
     <div class="flex gap-3">
-        <h2 class="karla font-bold">Cardinal</h2>
+        <h2 class="karla font-bold no-outline">Cardinal</h2>
         <img style="margin-top: -4px" width="64px" src="{Cardinal}" alt="logo">
         
     </div>
